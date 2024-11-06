be180dcd46ba2c44323125e6da98ed27c742dce1
maxamuudmuxiidin <maxamuudmuxiidin@gmail.com>
2024-11-06 14:55:55 +0300

    createTask

--- controllers/TaskControllers.js
+++ controllers/TaskControllers.js
@@ -9,3 +9,47 @@ exports.getTasks = (req, res) => {
     res.writeHead(200, { 'content-type': 'application/json'})
     res.end(JSON.stringify(tasks))
 }
+
+exports.createTask = (req, res) => {
+    const form = new IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            res.writeHead(400, { 'content-type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Error parsing form'
+            }))
+            return;
+        }
+
+        if (!fields.title) {
+            res.writeHead(400, { 'content-type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Title is required'
+            }))
+            return;
+        }
+
+        const image = files.image ? files.image[0] : null;
+
+        const tasks = readTasksFromFile()
+
+        const newTask = {
+            id: Date.now(),
+            title: fields.title,
+            description: fields?.description || '',
+            status: fields?.status || 'pending',
+            image: image ? `/uploads/${image.originalFilename}` : null,
+        }
+
+        tasks.push(newTask);
+
+        writeTasksToFile(tasks);
+
+        if(image) {
+            fs.copyFileSync(image.filepath, path.join(__dirname, '../uploads', image.originalFilename));
+        }
+
+        res.writeHead(200, { 'content-type': 'application/json'});
+        res.end(JSON.stringify(newTask))
+    })
+}
